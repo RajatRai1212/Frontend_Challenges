971afd5f980d98966f85af4c63d3dbec8ed30653
Rajatrai1212 <rajatrai1212@gmail.com>
2023-02-24 22:29:11 +0530

    first challenge

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import qr from './images/qr.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hero">
+        <img src = {qr} alt=" scaner" />
+        <h4>Improve your front-end skills by building projects</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
