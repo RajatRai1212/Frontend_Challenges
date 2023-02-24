5e607015c6de6bd6da90e13cfe0edc39856b177f
Rajatrai1212 <rajatrai1212@gmail.com>
2023-02-24 23:09:27 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
       <div className="hero">
         <img src = {qr} alt=" scaner" />
         <h4>Improve your front-end skills by building projects</h4>
+        <br />
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
     </div>
   );
